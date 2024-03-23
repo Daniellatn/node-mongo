ab663e0024ae835e56eb62f1a363d2fd89892020
Daniella Téu <daniellateun@gmail.com>
2024-03-22 21:22:15 -0300

    crud produtos

--- db/Comanda.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const schema = mongoose.Schema({
-  nome: String,
-})
-
-const Comanda = mongoose.model('Comanda', schema)
-
-module.exports = Comanda
--- routes/routes.js
+++ routes/routes.js
@@ -11,7 +11,19 @@ router.get('/produtos', async function(req, res) {
 })
 
 router.post('/produtos', async function(req, res) {
-  res.json(await Produto.create({nome: 'Pizza'}))
+  res.json(await Produto.create(req.body))
+})
+
+router.get('/produtos/:id', async function(req, res) {
+  res.json(await Produto.findById(req.params.id))
+})
+
+router.delete('/produtos/:id', async function(req, res) {
+  res.json(await Produto.findByIdAndDelete(req.params.id))
+})
+
+router.put('/produtos/:id', async function(req, res) {
+  res.json(await Produto.findByIdAndUpdate(req.params.id, req.body))
 })
 
 module.exports = router
